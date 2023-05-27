6241e6d551d1ead39a50526fe57a060f1e180619
GamercoderX <125307088+GamercoderX@users.noreply.github.com>
2023-05-27 22:06:58 +0100

    all done

--- thejs.js
+++ thejs.js
@@ -1,46 +1,114 @@
-function add(a,b){
-    return a + b
-
-}
-function subtract(a,b){
-    return a - b
-
-}
-function multiply(a,b){
-    return a * b
-
-}
-function division(a,b){
-    return a / b
-
+function add(a,b){ return a + b }
+function subtract(a,b){ return a - b }
+function multiply(a,b){ return a * b }
+function division(a,b){ 
+    if(b===0){
+        second = null
+        alert('cant divide by zero')
+        return '0'
+    }
+    return a / b 
 }
 
-function operate(a, op, b){
-    if(op === 'add'){
-       return add(a,b)
+
+
+function doEqual(){
+    
+    if(first!=null && second != null && oper != null){
+        // first = parseInt(first)
+        first = parseFloat(first)
+        second = parseFloat(second)
         
-    }
-    if(op === 'subtract'){
-        display.textContent = subtract(a,b)
+        if(oper === 'add'){
+            let answer = add(first,second)
+            display.textContent = answer
+            first = display.textContent
+            second = null
+            
+        }
+        if(oper === 'subtract'){
+            let answer = subtract(first,second)
+            display.textContent = answer
+            first = display.textContent
+            second = null
+            
+        }
+        if(oper === 'multiply'){
+            let answer = multiply(first,second)
+            display.textContent = answer
+            first = display.textContent
+            second = null
         
-    }
-    if(op === 'multiply'){
-        display.textContent = multiply(a,b)
-       
-    }
-    if(op === 'divide'){
-        display.textContent = division(a,b)
-    }
+        }
+        if(oper === 'divide'){
+            let answer = division(first,second)
+            display.textContent = answer
+            first = display.textContent
+            second = null
+            }
+            
+        }else{
+            console.log('nothing ran')
+        }
 }
+
+
+
+
 
 function changeDisplay(theid){
-    display.textContent += mydict[theid.id]
+    console.log('this function ran')
+
+    if (oper === null && first === null){
+        display.textContent = mydict[theid.id]
+        first = display.textContent
+
+    }else if (oper === null && first != null){
+        display.textContent += mydict[theid.id]
+        first = display.textContent
+    }else if (oper != null && second === null){
+        display.textContent = mydict[theid.id]
+        second = display.textContent
+
+    }else if (oper != null && second != null){
+        display.textContent += mydict[theid.id]
+        second = display.textContent
+    }
+    
+ 
+    
 }
 
-mydict = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10, 'zero': 0}
+function backspaceer(){
+
+    if(display.textContent != '0'){
+        display.textContent = display.textContent.slice(0, -1)
+    }
+
+    if(display.textContent === ''){
+        if (oper!=null){
+            second = null
+            display.textContent = '0'
+        } else{
+            first = null
+            display.textContent = '0'
+        }
+        
+    }
+    
+ 
+    
+}
 
 
-let theSum = []
+mydict = {'dot':'.','one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10, 'zero': 0}
+
+
+let first = null
+let oper = null
+let second = null
+let dotUsed = false
+
 
 const one = document.querySelector('#one');
 const two = document.querySelector('#two');
@@ -57,8 +125,11 @@ const plus = document.querySelector('#plus');
 const minus = document.querySelector('#minus');
 const divide = document.querySelector('#divide');
 const times = document.querySelector('#times');
+const dot = document.querySelector('#dot');
 
 const equals = document.querySelector('#equals');
+const back = document.querySelector('#backspace');
+const clear = document.querySelector('#clear');
 
 one.addEventListener('click', function() {changeDisplay(one)
     console.log(`you clicked ${one.id}`)});
@@ -80,62 +151,121 @@ nine.addEventListener('click', function() {changeDisplay(nine)
     console.log(`you clicked ${nine.id}`)});
 zero.addEventListener('click', function() {changeDisplay(zero) 
     console.log(`you clicked ${zero.id}`)});
+dot.addEventListener('click', function() {
+    if (dotUsed === false){
+    changeDisplay(dot)
+    dotUsed = true
+    }else{
+        alert('only one decimal point per number allowed 🙄')
+    }
+    
+    console.log(`you clicked ${dot.id}`)});
 
 plus.addEventListener('click', function() {
-    theSum.push(parseInt(display.textContent))
-    theSum.push('add')
-    display.textContent = undefined
-
+    doEqual()
+    oper = 'add'
+    dotUsed = false
+    
 });
 
 minus.addEventListener('click', function() {
-    theSum.push(parseInt(display.textContent))
-    theSum.push('minus')
-    display.textContent = undefined
-
+    doEqual()
+    oper = 'subtract'
+    dotUsed = false
+    
 });
 
 divide.addEventListener('click', function() {
-    theSum.push(parseInt(display.textContent))
-    theSum.push('divide')
-    display.textContent = undefined
+    doEqual()
+    oper = 'divide'
+    dotUsed = false
+    
 
 });
 times.addEventListener('click', function() {
-    theSum.push(parseInt(display.textContent))
-    theSum.push('times')
-    display.textContent = undefined
+    doEqual()
+    oper = 'multiply'
+    dotUsed = false
+    
 
 });
 
 
 
-equals.addEventListener('click', function() {
-    theSum.push(parseInt(display.textContent))
-    console.log(theSum)
-    for(i=0; i<theSum.length; i++){
-        if(theSum[i] === 'add'){
-            theSum[i+1]+=theSum[i-1]
-            
-        }
-        if(theSum[i] === 'minus'){
-            theSum[i+1] = theSum[i-1]-theSum[i+1]
-            
-        }
-        if(theSum[i] === 'divide'){
-            theSum[i+1] = theSum[i-1]/theSum[i+1]
-            
-        }
-        if(theSum[i] === 'times'){
-            theSum[i+1] = theSum[i-1]*theSum[i+1]
-            
-        }
+equals.addEventListener('click', doEqual);
+back.addEventListener('click', backspaceer);
+
+
+clear.addEventListener('click', function() {
+    first = null
+    second = null
+    oper = null
+    display.textContent = '0'
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === '/') {
+      // Perform some action when the forward slash key is pressed
+        doEqual()
+        oper = 'divide'
+        dotUsed = false
+      console.log('Forward slash key pressed!');
     }
-    console.log(theSum)
-    display.textContent = `final answer: ${theSum[theSum.length-1]}`
+  });
 
-    console.log(`you clicked ${equals.id}`)
-});
+  document.addEventListener('keydown', function(event) {
+    if (event.key === '*') {
+      // Perform some action when the forward slash key is pressed
+        doEqual()
+        oper = 'multiply'
+        dotUsed = false
+      console.log('multiply key pressed!');
+    }
+  });
 
+  document.addEventListener('keydown', function(event) {
+    if (event.key === '-') {
+      // Perform some action when the forward slash key is pressed
+        doEqual()
+        oper = 'subtract'
+        dotUsed = false
+      console.log('Subtract key pressed!');
+    }
+  });
 
+  document.addEventListener('keydown', function(event) {
+    if (event.key === '+') {
+      // Perform some action when the forward slash key is pressed
+        doEqual()
+        oper = 'add'
+        dotUsed = false
+      console.log('Addition key pressed!');
+    }
+  });
 
+  var dict = {
+    '0': zero,
+    '1': one,
+    '2': two,
+    '3': three,
+    '4': four,
+    '5': five,
+    '6': six,
+    '7': seven,
+    '8': eight,
+    '9': nine
+  };
+  
+  for (var key in dict) {
+    (function(key) {
+      document.addEventListener('keydown', function(event) {
+        if (event.key === key) {
+            changeDisplay(dict[key])
+          // Perform some action when the corresponding number key is pressed
+          console.log('Number key pressed: ' + dict[key]);
+        }
+      });
+    })(key);
+  }
+  
+  
